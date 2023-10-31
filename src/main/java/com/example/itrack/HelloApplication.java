6a23cb5c9aa49537e73b5ab8a9e76782f54f5223
Brock Whitson <w0781391@myscc.ca>
2023-10-31 19:42:27 -0400

    2 Menu options have been created with close and founder names added as items

--- src/main/java/com/example/itrack/HelloApplication.java
+++ src/main/java/com/example/itrack/HelloApplication.java
@@ -4,6 +4,9 @@ import com.example.itrack.Constants.ScreenRatio;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -16,7 +19,24 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
        BorderPane root = new BorderPane();
+        //add menu bar
+        MenuBar menu = new MenuBar();
+        Menu fileMenu = new Menu("File");
+        Menu creditsMenu = new Menu("Credits");
+        MenuItem exit = new MenuItem("Close Application");
+        exit.setOnAction(e->{
+            System.exit(0);
+        });
 
+        //credits
+        MenuItem founderOne = new MenuItem("Ali Mehdi");
+        MenuItem founderTwo = new MenuItem("Brock Whitson");
+        //add credits to tab
+        creditsMenu.getItems().addAll(founderOne,founderTwo);
+
+        fileMenu.getItems().addAll(exit);
+        menu.getMenus().addAll(fileMenu,creditsMenu);
+        root.setTop(menu);
         //Mayo De Noche
         Scene scene = new Scene(root,ScreenRatio.SCREEN_WIDTH, ScreenRatio.SCREEN_HEIGHT);
         stage.setTitle("iTrack");
