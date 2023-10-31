992e9e24232fd526dcda4870bad8c6cca987fef8
Brendon Mehdi <brendonmehdi@gmail.com>
2023-10-31 19:44:15 -0400

    MADE TRACKER PANE

--- src/main/java/com/example/itrack/Form.java
+++ src/main/java/com/example/itrack/Form.java
@@ -1,22 +1,34 @@
 package com.example.itrack;
 
 import com.example.itrack.Constants.ScreenRatio;
+import com.example.itrack.tabs.TrackerTab;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Form extends Application {
 
     // Hey abba
     //Hello brock
     @Override
     public void start(Stage stage) throws IOException {
        BorderPane root = new BorderPane();
+//create tabPane
+        TabPane tabPane = new TabPane();
+        //Create the tabs
 
+//        AddItemTab addItemTab = new AddItemTab();
+        TrackerTab addItemTab = TrackerTab.getInstance();//replaced with singleton form so we dont have to create new objected we commented out above
+        addItemTab.setClosable(false);
+
+
+        tabPane.getTabs().addAll(addItemTab);
+
+        root.setBottom(tabPane);
         //Mayo De Noche
         Scene scene = new Scene(root,ScreenRatio.SCREEN_WIDTH, ScreenRatio.SCREEN_HEIGHT);
         stage.setTitle("iTrack");
