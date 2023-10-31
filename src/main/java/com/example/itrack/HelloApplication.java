425eb26cd9c005f26b8243733604db7411f57f23
Brock Whitson <w0781391@myscc.ca>
2023-10-31 18:18:44 -0400

    Fixing Bugs

--- src/main/java/com/example/itrack/HelloApplication.java
+++ src/main/java/com/example/itrack/HelloApplication.java
@@ -3,16 +3,21 @@ package com.example.itrack;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    // Hey abba
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+       BorderPane root = new BorderPane();
+
+
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("iTrack");
         stage.setScene(scene);
         stage.show();
     }
