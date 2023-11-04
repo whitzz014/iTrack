7917b8b83d758ae101827006e644a2b7cfcf7907
Brock Whitson <w0781391@myscc.ca>
2023-11-04 18:52:35 -0400

    Database class added

---
+++ src/main/java/com/example/itrack/database/Database.java
@@ -0,0 +1,17 @@
+package com.example.itrack.database;
+
+import java.net.ConnectException;
+import java.nio.channels.ConnectionPendingException;
+
+public class Database {
+    /**
+     * Singleton Design Pattern
+     */
+
+    private static Database instance;
+    //make Connection
+    
+
+    private Database() {}
+
+}
